c5ff96dc4ecfb659837146d7dfb7d96814d5350a
Cheshta Verma <ashtcheshta03@gmail.com>
2026-03-13 18:13:13 +0530

    Implemented lower,upper,title,strip

--- programs/string_methods.py
+++ programs/string_methods.py
@@ -1,8 +1,8 @@
 #capitalize()
-# Definition: Converts the first character to uppercase.
+# Definition: Converts the first character to uppercase and rest to lowercase.
 # Use: Formatting sentences.
 
-s = "python programming"
+s = "python PROGRAMming"
 print(s.capitalize())
 
 '''Output:
@@ -10,12 +10,12 @@ Python programming
 '''
 
 
-'''
-casefold()
-Definition: Converts string to lowercase for case-insensitive comparison.
 
-Use: Useful for comparing strings regardless of case.
-'''
+#casefold()
+#Definition: Converts string to lowercase for case-insensitive comparison.
+
+#Use: Useful for comparing strings regardless of case.
+
 
 a = "HELLOOO"
 print(a.casefold())
@@ -25,14 +25,14 @@ Output:
 hellooo
 '''
 
-'''
-count(substring)
-Definition: Counts occurrences of a substring.
-'''
+
+# count(substring)
+# Definition: Counts occurrences of a substring.
+ 
 b = "banana and apple"
 print(b.count("a"))
 
-'''Output: 5
+''' Output: 5
 '''
 
 
@@ -41,4 +41,64 @@ print(b.count("a"))
 
 c = "Hello , how are you"
 print(c.endswith("ou"))
-# output: True
+'''
+Output: True
+'''
+
+# upper()
+# Definition:
+# Converts all characters of a string to uppercase.
+
+# Use:
+# Used when you want text in capital letter
+
+d = "hello world"
+print(d.upper())
+
+'''
+Output:
+HELLO WORLD
+'''
+
+# lower()
+# Definition:
+# Converts all characters of a string to lowercase.
+
+# Use:
+# Used to normalize text for comparison.
+
+e = "HELLO"
+print(e.lower())
+
+'''Output:
+hello
+'''
+
+# title()
+# Definition:
+# Converts the first letter of every word to uppercase.
+
+# Use:
+# Used for headings or titles.
+
+f = "python programming language"
+print(f.title())
+
+'''Output
+Python Programming Language
+'''
+
+
+# strip()
+# Definition:
+# Removes spaces from the beginning and end of a string.
+
+# Use:
+# Cleaning user input.
+
+s = "        -hello-          "
+print(s.strip())
+
+'''Output:
+-hello-
+'''
