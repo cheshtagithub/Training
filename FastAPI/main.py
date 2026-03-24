d81a7ff4c5af3bcb6b9a61b7f6191d714eedaecb
Cheshta Verma <ashtcheshta03@gmail.com>
2026-03-24 18:21:03 +0530

    Practiced data validation using pydantic

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -56,36 +56,92 @@
 #     deleted = users.pop(user_index)
 #     return {"message": f"{deleted} deleted"}
 
+# @app.get("/")
+# def home():
+#     return{"message": "Welcome to FastAPI"}
+
+# @app.get("/hello")
+# def hello():
+#     return{"message": "Hello World"}
+
+# products = ["Laptop", "Phone", "Tablet"]
+
+# @app.get("/products")
+# def product_list():
+#     return {"products": products}
+
+# @app.post("/products")
+# def create_product(name: str):
+#     products.append(name)
+#     return {"message": "Product added", "products": products}
+
+# @app.put("/products/{product_index}")
+# def update_product(product_index: int, name: str):
+#     products[product_index] = name
+#     return {"message": "Product updated", "products": products}
+
+# @app.delete("/products/{product_index}")
+# def delete_product(product_index: int):
+#     deleted = products.pop(product_index)
+#     return {"message": f"{deleted} deleted"}
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel, Field
+
+# app = FastAPI()
+
+# users = []
+
+# class User(BaseModel):
+#     name: str
+#     email: str
+#     age: int = Field(gt=0)
+#     is_active: bool = True
+
+
+# @app.post("/users")
+# def create_user(user: User):
+#     users.append(user)
+
+#     return {
+#         "message": "User added",
+#         "user": user
+#     }
+
+# @app.get("/users")
+# def get_users():
+#     return {"users": users}
 
 from fastapi import FastAPI
+from pydantic import BaseModel, Field, EmailStr
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return{"message": "Welcome to FastAPI"}
+users = []
 
-@app.get("/hello")
-def hello():
-    return{"message": "Hello World"}
+class User(BaseModel):
+    name: str = Field(min_length=3, max_length=50)
+    email: EmailStr
+    age: int = Field(gt=0, lt=100)
+    phone: str = Field(min_length=10, max_length=10)
+    password: str = Field(min_length=8)
+    is_active: bool = True
 
-products = ["Laptop", "Phone", "Tablet"]
 
-@app.get("/products")
-def product_list():
-    return {"products": products}
+@app.post("/users")
+def create_user(user: User):
 
-@app.post("/products")
-def create_product(name: str):
-    products.append(name)
-    return {"message": "Product added", "products": products}
+    users.append(user.model_dump())
 
-@app.put("/products/{product_index}")
-def update_product(product_index: int, name: str):
-    products[product_index] = name
-    return {"message": "Product updated", "products": products}
+    return {
+        "message": "User added successfully",
+        "user": user
+    }
 
-@app.delete("/products/{product_index}")
-def delete_product(product_index: int):
-    deleted = products.pop(product_index)
-    return {"message": f"{deleted} deleted"}
+
+@app.get("/users")
+def get_users():
+
+    return {
+        "users": users
+    }
