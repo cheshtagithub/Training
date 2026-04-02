ea6362151ed6e360ba6a35a1e8524b2c9d68db6d
Cheshta Verma <ashtcheshta03@gmail.com>
2026-04-02 15:12:40 +0530

    Created program to add and delete from a list

--- reactjs/javascript/tasks/task5.js
+++ reactjs/javascript/tasks/task5.js
@@ -11,9 +11,53 @@
 // a.style.color = "purple"
 // a.style.backgroundColor = "black";
 
+// let flag = 0;
 
-let bulb = document.querySelector("#bulb");
-let btn = document.querySelector("button");
-btn.addEventListener("click", function(){
-    bulb.style.backgroundColor = "yellow";
-})
+// let bulb = document.querySelector("#bulb");
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     if(flag == 0){
+//         bulb.style.backgroundColor = "yellow";
+//         flag = 1;
+//     }
+//     else{
+//         bulb.style.backgroundColor = "transparent";
+//         flag = 0;
+//     }
+// })
+
+// let h = document.querySelector("h1");
+// let btn = document.querySelector("button");
+// let b = document.querySelector("body");
+// let para = document.querySelector("p");
+// let list = document.querySelector("ul")
+// btn.addEventListener("click", function(){
+//     let new_item = document.createElement("li");
+//     new_item.textContent = "orange";
+//     list.appendChild(new_item)
+// })
+
+let input = document.querySelector("#taskInput");
+let button = document.querySelector("#addBtn");
+let list = document.querySelector("#taskList");
+
+button.addEventListener("click", function () {
+
+    let taskText = input.value;
+
+    let newTask = document.createElement("li");
+
+    newTask.textContent = taskText;
+
+    list.appendChild(newTask);
+
+    input.value = "";
+
+
+    newTask.addEventListener("click", function(){
+        newTask.remove();
+});
+
+});
+
