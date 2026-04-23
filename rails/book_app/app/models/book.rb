12c4899ecf52c86374575d074b2173590162f458
Cheshta Verma <ashtcheshta03@gmail.com>
2026-04-23 16:03:34 +0530

    Added validations and callback

--- rails/book_app/app/models/book.rb
+++ rails/book_app/app/models/book.rb
@@ -1,2 +1,21 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence:true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+
+  validates :description, length: { minimum: 10 }
+  validates :genre, presence: true
+
+  before_save :capitalize_title
+  before_validation :set_default_genre
+
+  private
+
+  def capitalize_title
+    self.title = title.titleixe
+  end
+
+  def set_default_genre
+    self.set_default_genre = "General" if genre.blank    
+  end
 end
