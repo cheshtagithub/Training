fae6a78454160ea9662fe8d9e55c32dfe01b9c9d
Cheshta Verma <ashtcheshta03@gmail.com>
2026-04-29 14:48:20 +0530

    Created login page

--- rails/task_manager/app/controllers/application_controller.rb
+++ rails/task_manager/app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :current_user
+
+  def current_user
+    current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      redirect_to "/login", alert: "Please login first"
+    end
+  end
+
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 end
