cbd8067c7fcf522252a9bbf207d09c886085a6b0
Cheshta Verma <ashtcheshta03@gmail.com>
2026-05-05 12:03:08 +0530

    Added valid email and password restriction/validations

--- rails/notes_app/app/models/user.rb
+++ rails/notes_app/app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :notes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_many :notes, dependent: :destroy
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be valid email" }
+
+  validates :password,
+    format: {
+      with: /\A(?=.*[a-zA-Z])(?=.*\d).{8,}\z/,
+      message: "must be at least 8 characters, include letters and numbers"
+    },
+    if: -> { password.present? }  
 end
